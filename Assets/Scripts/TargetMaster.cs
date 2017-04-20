29acc93cf992a45f96c02855e83737efcc139833
Victor Bodell <vbodell@kth.se>
2017-04-20 13:19:59 +0200

    Add TargetMaster-script
    Script should check all targets amongst children and enter next level/scene

---
+++ Assets/Scripts/TargetMaster.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Use with Target Master, A master object with Targets as children
+public class TargetMaster : MonoBehaviour {
+
+	private GameObject[] Targets;
+	private bool[] hits;
+
+	// Use this for initialization
+	void Start () {
+
+		Targets = GetComponentsInChildren<GameObject> ();
+
+	}
+	
+	// Update is called once per frame
+	// SHOULD NOT BE UPDATED EVERY FRAME??
+	void Update () {
+
+		//Get all current hits for targets on level
+		for(int i = 0; i < Targets.Length; i++){
+			TargetCollision targScript = Targets [i].GetComponent<TargetCollision> ();
+			hits [i] = targScript.hit;
+		}
+
+		//Check if all are hit (Optimize later)
+		bool nextLevel = false;
+		foreach(bool h in hits){
+			if (!h) {
+				nextLevel = false;
+				break;
+			} else {
+				nextLevel = true;
+			}
+		}
+
+		//if all are hit go to next scene
+		if (nextLevel) {
+			//next scene code here...
+		}
+
+	}
+}
