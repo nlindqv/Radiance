b03aa9fcc04e33ff7495a203e015ec6c380d52ca
Victor Bodell <vbodell@kth.se>
2017-04-20 11:48:19 +0200

    add Target and Targetscript for collision

---
+++ Assets/Scripts/TargetCollision.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCollision : MonoBehaviour
+{
+	private Renderer rend;
+	public Color neutral;
+	public Color targetHit;
+
+	public bool hit;
+
+	void Start()
+	{
+		//Get the renderer and enable rendering
+		rend = GetComponentInChildren<Renderer>();
+		rend.enabled = true;
+
+		//Upon start 
+		hit = false;
+
+		Debug.Log ("Color initiated");
+		//Assign the neutral color
+		Material mat = rend.material;
+		mat.color = neutral;
+
+	}
+
+	//Target is hit
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.name == "ray") {
+			Material mat = rend.material;
+			mat.color = targetHit;
+			hit = true;
+		}
+	}
+
+	void Update() {
+		if (!hit) {
+			Material mat = rend.material;
+			mat.color = neutral;
+		}
+		hit = false;
+	}
+
+	/*
+	 * void HandleLaserCollision (Gameobject ray)
+	{
+		Material mat = rend.material;
+		mat.color = targetHit;
+		hit = true;
+	}
+
+	void Update()
+	{
+		hit = false;
+		
+	}
+	 * /
+
+	//Target is no longer hit
+	void OnCollisionExit (Collision col)
+	{
+		if (col.gameObject.name == "ray") {
+			Material mat = rend.material;
+			mat.color = neutral;
+			hit = false;
+		}
+	}
+
+
+}
+*/
+}
