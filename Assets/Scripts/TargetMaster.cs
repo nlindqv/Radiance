e8f7bf53b5f316fb8693350b514923cd477477d6
Victor Bodell <vbodell@kth.se>
2017-04-20 13:51:22 +0200

    update TargetMaster-script, works for group of 1 target

--- Assets/Scripts/TargetMaster.cs
+++ Assets/Scripts/TargetMaster.cs
@@ -5,26 +5,31 @@ using UnityEngine;
 //Use with Target Master, A master object with Targets as children
 public class TargetMaster : MonoBehaviour {
 
-	private GameObject[] Targets;
+	private TargetCollision[] Targets;
 	private bool[] hits;
 
 	// Use this for initialization
 	void Start () {
 
-		Targets = GetComponentsInChildren<GameObject> ();
+		Targets = GetComponentsInChildren<TargetCollision> ();
+		hits = new bool[Targets.Length];
+		Debug.Log ("Get all children");
+		//Debug.Log ();
 
 	}
 	
 	// Update is called once per frame
 	// SHOULD NOT BE UPDATED EVERY FRAME??
 	void Update () {
-
+		
+		Debug.Log ("Getting all " + Targets.Length + " hits.");
 		//Get all current hits for targets on level
 		for(int i = 0; i < Targets.Length; i++){
-			TargetCollision targScript = Targets [i].GetComponent<TargetCollision> ();
+			TargetCollision targScript = Targets [i];
 			hits [i] = targScript.hit;
-		}
 
+		}
+		Debug.Log ("Checking bools");
 		//Check if all are hit (Optimize later)
 		bool nextLevel = false;
 		foreach(bool h in hits){
@@ -39,6 +44,7 @@ public class TargetMaster : MonoBehaviour {
 		//if all are hit go to next scene
 		if (nextLevel) {
 			//next scene code here...
+			Debug.Log("Entering next scene");
 		}
 
 	}
