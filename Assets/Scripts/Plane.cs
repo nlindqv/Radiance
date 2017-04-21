2d719ebdfb437c4988baa6a0215ebdf867a755ff
Senad M. Delic <senad.matuh@gmail.com>
2017-04-21 16:57:55 +0200

    refactorerar kod för touchInput samt plan-objekt

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -2,15 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Klass används för att fånga upp spelplanen vid styrning av TouchInput 
+/// </summary>
 public class Plane : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
