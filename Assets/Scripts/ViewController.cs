76e3866b86f46b35c21dd637c0c0091895ef92be
Niklas Lindqvist <niklas.lindqvist96@gmail.com>
2017-04-24 11:04:42 +0200

    Updated toggle-script

--- Assets/Scripts/ViewController.cs
+++ Assets/Scripts/ViewController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ViewController : Selectable {
+public class ViewController : MonoBehaviour {
 	public static bool gameMode = true;
+	Text text;
 	// Use this for initialization
 	void Start () {
-		
+		text = GetComponentInChildren<Button> ().gameObject.GetComponentInChildren<Text>();
 	}
 	
 	// Update is called once per frame
@@ -15,7 +16,8 @@ public class ViewController : Selectable {
 		
 	}
 	public void Click (){
-
+		if (gameMode) text.text = "Mirror Mode";
+		else text.text = "Laser Mode";
 		gameMode = !gameMode;
 		Debug.Log ("Gamemode: " + gameMode);
 	}
