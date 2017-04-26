85d60d596ca6c8d00176772830f7393b75f664e0
Victor Bodell <vbodell@kth.se>
2017-04-26 14:30:34 +0200

    Add script for Colorchanger/gate

---
+++ Assets/Scripts/Specific Objects/ColorChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+
+	//color of gate
+	public Color color;
+
+	//geometry variables for gate
+//	private Vector3 position;
+//	private float height;
+//	private float width;
+
+	// Use this for initialization
+	void Start () {
+//		position = transform.position;
+//		height = transform.lossyScale.x;
+//		width = transform.lossyScale.z;
+
+		//Assign gate color, render must be enabled
+		GetComponent<Renderer>().enabled = true;
+		GetComponent<Renderer>().material.color = color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void HandleLaserCollision(LaserRay ray){
+		if(ray.Color.Equals(this.color)){
+			Vector3 direction = ray.HitPoint - ray.transform.position;
+			LaserRay newRay = ray;
+			newRay.Color = color;
+			Instantiate (newRay, ray.HitPoint, Quaternion.LookRotation(direction));
+		}
+	}
+}
