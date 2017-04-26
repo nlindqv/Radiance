3bf700a77c71a703640b511dbdd3d5b0ae7e1d74
Sara Ersson <sara.ersson@gmail.com>
2017-04-26 15:35:36 +0200

    change gate script

--- Assets/Scripts/Specific Objects/Gate.cs
+++ Assets/Scripts/Specific Objects/Gate.cs
@@ -6,14 +6,9 @@ public class Gate : MonoBehaviour, IInteractables {
 	public GameObject ray;
 	public bool hit;
 	public Color gateColor;
-	// public Color laserColor;    for debugging
 
 	// Use this for initialization
 	void Start () {
-		hit = false;
-	//	laserColor = ray.GetComponent<LaserRay> ().Color;
-	//	gateColor = new Color(3,5,236,0);
-
 	}
 	
 	// Update is called once per frame
@@ -22,18 +17,13 @@ public class Gate : MonoBehaviour, IInteractables {
 	}
 
 	public void HandleLaserCollision(LaserRay laserHit){
-		Debug.Log (laserHit.Color);
-		Debug.Log (gateColor);
-	//	if (laserHit.Color.Equals(gateColor)) {
-			hit = true;
+		if (laserHit.Color.Equals(gateColor)) {
 			Vector3 direction = laserHit.transform.forward;
 			GameObject newRay = Instantiate (ray, laserHit.HitPoint, Quaternion.LookRotation (laserHit.transform.forward));
 			Transform parentTranform = laserHit.transform.parent;
 			newRay.transform.parent = parentTranform;
 			LaserRay newLaser = newRay.GetComponent<LaserRay> ();
 			newLaser.Color = gateColor;
-
-
-		//}
+			}
 	}
 }
