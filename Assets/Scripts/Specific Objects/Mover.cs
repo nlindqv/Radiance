a9eab99391a0d9d464268a96042b1a372d99d46d
Victor Bodell <vbodell@kth.se>
2017-04-27 10:26:01 +0200

    Add Mover-model, -scene & -script

---
+++ Assets/Scripts/Specific Objects/Mover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	Vector3 prevTransform;
+//	float z;
+
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	void OnTriggerEnter(Collider Other)
+	{
+		prevTransform = Other.gameObject.transform.position;
+//		z = transform.localScale.z;
+
+	}
+
+	void OnTriggerStay(Collider Other)
+	{
+		prevTransform = Other.gameObject.transform.position;
+		Other.gameObject.transform.position = Vector3.MoveTowards (prevTransform, transform.position, 0.1f);
+	}
+}
