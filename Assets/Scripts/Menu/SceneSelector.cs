70415bc2033ea988d74323c5c569d899c506ded4
Sara Ersson <sara.ersson@gmail.com>
2017-04-27 15:07:19 +0200

    add start menu object and scene selector script

---
+++ Assets/Scripts/Menu/SceneSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector: MonoBehaviour {
+
+	public void LoadSceneByName (string SceneName) 
+	{
+		SceneManager.LoadScene (SceneName);
+
+	}
+}
