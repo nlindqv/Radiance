b2e23f99b92de23ee71babe7bc792e7456804571
Benedictus <benedith@kth.se>
2017-04-28 19:21:44 +0200

    mover features for line and square

---
+++ Assets/Scripts/Specific Objects/moveOnpath.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveOnpath : MonoBehaviour
+{
+
+	public pathControl pathFollow;
+
+	public int CurrentId = 0;
+	public float v;
+	private float reachDis = 1.0f; 
+	public float rotSpeed = 5.0f;
+	int end;
+
+	public string pathName;
+	Vector3 last_pos;
+	Vector3 curr;
+	// Use this for initialization
+	void Start ()
+	{
+		//pathFollow = GameObject.Find (pathName).GetComponent<pathControl> ();
+		end = pathFollow.paths.Count;
+		last_pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float distance = Vector3.Distance (pathFollow.paths [CurrentId].position, transform.position);
+		transform.position = Vector3.MoveTowards (transform.position, pathFollow.paths [CurrentId].position, Time.deltaTime * v);
+//		var rotation = Quaternion.LookRotation (pathFollow.paths [CurrentId].position - transform.position);
+//		transform.rotation = Quaternion.Slerp (transform.rotation, Time.deltaTime * v);
+		if (distance <= reachDis) 
+		{
+			CurrentId++;
+		} 
+		//Debug.Log ();
+		if( CurrentId >= end)
+		{
+			//pathFollow.paths.Reverse ();
+			CurrentId = 0;
+			transform.position = Vector3.MoveTowards (transform.position, pathFollow.paths [CurrentId].position, Time.deltaTime * v);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Specific Objects/pathControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pathControl : MonoBehaviour 
+{
+	public Color rayColor = Color.white;
+	public List<Transform> paths = new List<Transform>();
+	private float reachDis = 1.0f; 
+	Transform [] pos;
+	public int CurrentId = 0;
+	void OnDrawGizmos()
+	{
+		Gizmos.color = rayColor;
+		pos = GetComponentsInChildren<Transform> ();
+		paths.Clear ();
+
+		foreach (Transform path in pos)
+		{
+			if(path != this.transform)
+			{
+				paths.Add(path);
+			}
+		}
+
+		for(int i = 0; i < paths.Count; i++)
+		{
+			Vector3 position = paths [i].position;
+			if (i > 0) {
+				Vector3 previous = paths [i - 1].position;
+				Gizmos.DrawLine (previous, position);
+				Gizmos.DrawWireSphere (position,0.3f);
+			}
+		}
+	}
+
+		
+}
