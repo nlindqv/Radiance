712c13ad3ad62eac5d43831d71d73f6634fccff0
Victor Bodell <vbodell@kth.se>
2017-05-08 13:11:50 +0200

    Update memory

--- Assets/Scripts/DataObjects/LevelData.cs
+++ Assets/Scripts/DataObjects/LevelData.cs
@@ -1,8 +1,78 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[Serializable]
 public class LevelData{
-	public int [] tutorialIndex;
-	public string [] levelName;
+
+
+//	public int [] tutorialIndex;
+//	public string [] levelName;
+
+	public int levelIndex;
+	//	public int LevelIndex { get { return levelIndex;} }
+
+	public int starCount;
+
+	public string levelName;
+	//	public string LevelName { get { return levelName;} }
+
+	public int tutorialIndex;
+
+	public LevelData(){
+		this.levelIndex = -1;
+		this.starCount = 3;
+		this.levelName = "testing";
+		tutorialIndex = -1;
+	}
+
+	public LevelData(int levelIndex){
+		this.levelIndex = levelIndex;
+		this.levelName = "Level" + levelIndex;
+		starCount = 3;
+		tutorialIndex = -1;
+	}
+
+	public LevelData(int levelIndex, string levelName){
+		this.levelIndex = levelIndex;
+		this.levelName = levelName;
+		starCount = 3;
+		tutorialIndex = -1;
+	}
+
+	public LevelData(int levelIndex, string levelName, int starCount){
+		this.levelIndex = levelIndex;
+		this.levelName = levelName;
+		this.starCount = starCount;
+		tutorialIndex = -1;
+	}
+
+	public override string ToString(){
+		string s = "Index: " + this.levelIndex.ToString () + " w/ stars: " + this.starCount.ToString() + " & name " + this.levelName;
+		return s;
+	}
+	/*
+	public class Comparer<LevelData> : IComparer<LevelData>
+	{
+		private LevelData comparer;
+		public Comparer(Func<T, T, int> comparer)
+		{
+			this.comparer = comparer;
+		}
+		public static IComparer<T> Create(Func<T, T, int> comparer)
+		{
+			return new FunctionalComparer<T>(comparer);
+		}
+		public int Compare(T x, T y)
+		{
+			return comparer(x, y);
+		}
+	}*/
+}
+
+[Serializable]
+public class LevelList{
+	public List<LevelData> list = new List<LevelData>();
 }
--- Assets/Scripts/General/MemoryManager.cs
+++ Assets/Scripts/General/MemoryManager.cs
@@ -2,14 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.SceneManagement;
 
 public class MemoryManager : MonoBehaviour {
 
-	LevelData[] levels;
+
+	private static LevelList LEVELS = 
+		JsonUtility.FromJson<LevelList> (File.ReadAllText (Application.dataPath + "/Resources/infoTest.json"));
+	
+	private static int levelIndexOffset = 3;
+
+	private static LevelData getLevel(){
+		int index = SceneManager.GetActiveScene ().buildIndex;
+		print (index);
+		return LEVELS.list [index-levelIndexOffset];
+	}
+
+	public static string LoadLevelName(){
+		// Load levelName from array in a JSon-file
+//		string json = File.ReadAllText (Application.dataPath + "/Resources/Levels.json");
+//		return JsonUtility.FromJson<LevelData> (json).levelName[SceneManager.GetActiveScene ().buildIndex - levelIndexOffset];
+//		return LEVELS.list[0].levelName;
+
+		return getLevel().levelName;
+	}
+
+	public static int LoadTutorialIndex(){
+		// Load tutorialIndex from array in a JSon-file
+//		string json = File.ReadAllText (Application.dataPath + "/Resources/Levels.json");
+//		return JsonUtility.FromJson<LevelData> (json).tutorialIndex [SceneManager.GetActiveScene ().buildIndex - levelIndexOffset];
+		return getLevel().tutorialIndex;
+	}
+
+
+
+	#region TestFunctions
 
 	public static void mem(){
-		writeData ("kult");
+		writeOnce ();
 		readData ();
+	}
+
+	public static void writeOnce(){
+		LevelList ls = new LevelList ();
+		for (int i = 1; i <= 20; i++) {
+			string name = "Level " + i;
+			LevelData t = new LevelData (i - 1, name);
+			ls.list.Add (t);
+		}
+		string path2write = "Assets/Resources/infoTest.json";
+		using (StreamWriter s = new StreamWriter (path2write)) {
+			s.Write(JsonUtility.ToJson(ls));
+		}
+
 	}
 
 	public static void writeData(string data){
@@ -55,5 +100,7 @@ public class MemoryManager : MonoBehaviour {
 				print (ld);
 		}
 	}
+
+	#endregion
 
 }
