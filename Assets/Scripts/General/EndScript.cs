84c679e095271099c1e70c9f80b82df4c01f43aa
Albin Bystrom <albinby@kth.se>
2017-05-14 19:00:44 +0200

    Add float away feature

---
+++ Assets/Scripts/General/EndScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScript : MonoBehaviour {
+
+    public static void FloatAway(Transform trans)
+    {
+        Rigidbody gameObjectsRigidBody = (trans.gameObject.GetComponent<Rigidbody>() == null) ? trans.gameObject.AddComponent<Rigidbody>() : trans.gameObject.GetComponent<Rigidbody>(); // Add the rigidbody.
+        if (gameObjectsRigidBody == null) return;
+        gameObjectsRigidBody.mass = UnityEngine.Random.Range(0.0f, 10.0f); // Set the GO's mass to 5 via the Rigidbody.
+        gameObjectsRigidBody.velocity = new Vector3(UnityEngine.Random.Range(-2.0f, 2.0f), UnityEngine.Random.Range(0.0f, 4.0f), UnityEngine.Random.Range(-2.0f, 2.0f));
+        gameObjectsRigidBody.isKinematic = false;
+        gameObjectsRigidBody.useGravity = false;
+        gameObjectsRigidBody.angularVelocity = new Vector3(UnityEngine.Random.Range(-2.0f, 2.0f), UnityEngine.Random.Range(-2.0f, 2.0f), UnityEngine.Random.Range(-2.0f, 2.0f)) ;
+        if (gameObjectsRigidBody.GetComponent<Movable>()) gameObjectsRigidBody.GetComponent<Movable>().enabled = false;
+        if (gameObjectsRigidBody.GetComponent<Movable>()) gameObjectsRigidBody.GetComponent<Rotate>().enabled = false;
+        gameObjectsRigidBody.freezeRotation = false;
+     }
+}
--- Assets/Scripts/General/GameManager.cs
+++ Assets/Scripts/General/GameManager.cs
@@ -1,218 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
-public class GameManager : MonoBehaviour
-{
-    public const string PAUSE_BTN_NAME = "PauseButton";
-    public enum GameMode { laserMode, mirrorMode, none };	// gameModes
-	public enum GameState {tutorial,gameRunning,endScreen, gamePaused};	// gameStates available
-
-    public static GameMode gameMode = GameMode.none;		// init gameMode to none
-    private static GameState prevGameState;					// save prev gameState
-	public GameObject laserRay;
-    [HideInInspector]
-    public ViewController UI;                               // UI containing all panels etc.
-    [HideInInspector]
-    public TargetMaster targetMaster;
-    [HideInInspector]
-    public LaserMode laserMode;
-
-	public static GameState gameState;
-	private string levelName;
-	private int tutorialIndex = 1;
-
-    private LaserStack laserStack;
-    private int numOfLasers = 20;
-
-	// Use this for initialization
-	void Start ()
-	{
-        /*
-			"Prata med minne"
-
-
-		levelName = loadName();
-		loadScore();
-		loadTutorialIndex();
-
-		*/
-        laserMode = GameObject.Find("LightSource").GetComponent<LaserMode>();
-
-        laserStack = new LaserStack();
-        Debug.Log(GameObject.FindObjectsOfType(typeof(IInteractables)));
-  
-        foreach (IInteractables inter in GameObject.FindObjectsOfType(typeof(IInteractables)))
-        {
-            inter.SetLasers(laserStack);
-            inter.SetLaser(laserRay.GetComponent<LaserRay>());
-        }
-        foreach (Transform trans in GameObject.FindObjectsOfType(typeof(Transform)))
-        {
-            Rigidbody gameObjectsRigidBody = trans.gameObject.AddComponent<Rigidbody>(); // Add the rigidbody.
-            if (gameObjectsRigidBody == null) continue; 
-            //gameObjectsRigidBody.mass = 5; // Set the GO's mass to 5 via the Rigidbody.
-            gameObjectsRigidBody.velocity = new Vector3(UnityEngine.Random.Range(-1.0f, 1.0f), 0, UnityEngine.Random.Range(-1.0f, 1.0f));
-            //gameObjectsRigidBody.MoveRotation( Quaternion.Euler(UnityEngine.Random.Range(-10.0f, 10.0f), UnityEngine.Random.Range(-0.0f, 10.0f), UnityEngine.Random.Range(-10.0f, 10.0f)))  ;
-            //floatAway(child);
-            //floatAway(trans);
-        }
-
-        generateLaserStack();
-
-        laserMode.laserStack = laserStack;
-       
-
-		// Start game with tutorial window #1
-		gameState = GameState.tutorial;
-        prevGameState = GameState.tutorial;
-        gameMode = GameMode.none;
-       
-		// Access UIs components and children
-		UI = GameObject.Find ("UI").GetComponent<ViewController> ();
-		// Access targetMaster
-		targetMaster = GameObject.Find ("TargetMaster").GetComponent<TargetMaster> ();
-        // Hide mellanmeny
-        UI.transform.Find("Canvas").transform.Find("MellanMeny").gameObject.SetActive(false);
-		// If totrial index = -1 dont show anything, otherwise load tutorial with index tutorialIndex
-		if (tutorialIndex >= 0) {
-			LoadTutorial (tutorialIndex);
-		}
-	}
-
-    private void generateLaserStack()
-    {
-        for (int i = 0; i < numOfLasers; i++)
-        {
-            LaserRay newLaser = Instantiate(laserRay, new Vector3(0, 0, 0), Quaternion.identity).GetComponent<LaserRay>();
-            laserStack.push(newLaser);
-        }
-    }
-
-    void FixedUpdate ()
-	{
-		// When entering gameRunning from tutorial, show gameModebutton
-        if(prevGameState == GameState.tutorial && gameState == GameState.gameRunning)
-        {
-            gameMode = GameMode.laserMode;
-            UI.ShowGameModeButton();
-        }
-		// Decide which code is running according to gameState
-		switch (gameState) {
-		// Case tutorial, do something...
-		case GameState.tutorial:
-            break;
-			// Case gameRunning, show gamemode-button and check if level is completed
-        case GameState.gameRunning:
-            //visa pause-knappen
-            UI.transform.Find(PAUSE_BTN_NAME).gameObject.SetActive(true);
-            UI.ShowGameModeButton();
-			CheckLevelCompleted ();
-			break;
-			// Case endScreen, check what next state is
-		case GameState.endScreen:
-			CheckNextState ();
-			break;
-		case GameState.gamePaused:
-			CheckNextState ();
-			break;
-
-		default:
-			break;
-		}
-        prevGameState = gameState; 
-	}
-
-	private void LoadTutorial (int index)
-	{
-		// Load info about tutorial
-		string title = "title";
-		string text = "sample text";
-		Image icon = null;
-		UI.NewTutorial (title, text, icon);
-	}
-
-	private void CheckLevelCompleted ()
-	{
-		// If all targets are hit, load endScreen and set gameMode = none
-		if (targetMaster.CheckLevelCompleted ()) {
-			LoadLevelEndScreen ();
-			gameState = GameState.endScreen;
-            gameMode = GameMode.none;
-		}
-	}
-
-	private void LoadLevelEndScreen ()
-	{
-		// Load info about which level got completed
-		string level = "Level";
-		UI.ShowEndScreen (level, targetMaster.GetCollectables ());
-	}
-
-	private void CheckNextState ()
-	{
-		// Check select to decide what next state is
-		switch (UI.select) {
-		case ViewController.Select.menu:
-			MainMenu ();
-			break;
-		case ViewController.Select.replay:
-			Replay ();
-			break;
-		case ViewController.Select.next:
-			NextScene ();
-			break;
-		default:
-			break;
-		}
-	}
-
-	private void NewScene (int n)
-	{ // where n is offset from current scene
-		Scene activeScene = SceneManager.GetActiveScene ();
-		SceneManager.LoadScene (activeScene.buildIndex + n);
-		Debug.Log ("Entering next scene");
-	}
-    	
-
-	private void NextScene ()
-	{
-		// Load next level
-		NewScene (1);
-		gameState = GameState.gameRunning;
-	}
-
-	private void Replay ()
-	{
-		// Load same level again
-		NewScene (0);
-		gameState = GameState.gameRunning;
-	}
-
-	private void MainMenu ()
-	{
-        SceneManager.LoadScene("StartScene");
-	}
-
-    private void floatAway(Transform transform)
-    {
-        foreach(Transform child in transform)
-        {
-          // .velocity =  new Vector3(UnityEngine.Random.Range(-10.0f, 10.0f), UnityEngine.Random.Range(-0.0f, 10.0f), UnityEngine.Random.Range(-10.0f, 10.0f));
-            Rigidbody gameObjectsRigidBody = child.gameObject.AddComponent<Rigidbody>(); // Add the rigidbody.
-            //gameObjectsRigidBody.mass = 5; // Set the GO's mass to 5 via the Rigidbody.
-            gameObjectsRigidBody.velocity = new Vector3(UnityEngine.Random.Range(-10.0f, 10.0f), UnityEngine.Random.Range(-0.0f, 10.0f), UnityEngine.Random.Range(-10.0f, 10.0f));
-            floatAway(child);
-        }
-    }
-
-}
-=======
-
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -267,12 +52,10 @@ public class GameManager : MonoBehaviour
 
         generateLaserStack();
 
-        laserMode.laserStack = laserStack;
-       
+        laserMode.laserStack = laserStack;       
 
-       
-		// Access UIs components and children
-		UI = GameObject.Find ("UI").GetComponent<ViewController> ();
+        // Access UIs components and children
+        UI = GameObject.Find ("UI").GetComponent<ViewController> ();
 		// Access targetMaster
 		targetMaster = GameObject.Find ("TargetMaster").GetComponent<TargetMaster> ();
 		// Hide mellanmeny
@@ -341,8 +124,8 @@ public class GameManager : MonoBehaviour
 			break;
 		}
         prevGameState = gameState;
-        RenderSettings.skybox.SetFloat("_Rotation", 2 * Time.deltaTime + RenderSettings.skybox.GetFloat("_Rotation"));
-        RenderSettings.skybox.SetFloat("_Exposure", Mathf.Sin(2 * Time.deltaTime + RenderSettings.skybox.GetFloat("_Rotation"))/8.0f + 1.2f);
+        RenderSettings.skybox.SetFloat("_Rotation", speed * Time.deltaTime + RenderSettings.skybox.GetFloat("_Rotation"));
+        RenderSettings.skybox.SetFloat("_Exposure", Mathf.Sin(2 * Time.deltaTime + RenderSettings.skybox.GetFloat("_Rotation"))/8.0f + 1.0f);
         //Debug.Log(skybox.GetFloat("_Exposure"));
         //RenderSettings.skybox = skybox;
 
@@ -372,14 +155,40 @@ public class GameManager : MonoBehaviour
 
 	private void LoadLevelEndScreen ()
 	{
-		// Load info about which level got completed
-		string level = "Level";
-		int score = targetMaster.GetCollectables ();
-		MemoryManager.WriteScore2Memory (score);
-		UI.ShowEndScreen (level, score);
-	}
+        // Load info about which level got completed
+        //EndScript.FloatAway(GameObject.FindObjectsOfType<IInteractables>());        
+        StartCoroutine(Order());        
+    }
+  
 
-	private void CheckNextState ()
+    IEnumerator Order()
+    {
+        foreach (IInteractables item in GameObject.FindObjectsOfType<IInteractables>())
+        {
+            EndScript.FloatAway(item.transform);
+        }
+        foreach (Target item in GameObject.FindObjectsOfType<Target>())
+        {
+            EndScript.FloatAway(item.transform);
+        }
+        foreach (LaserMode item in GameObject.FindObjectsOfType<LaserMode>())
+        {
+            EndScript.FloatAway(item.transform);
+        }
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag("FloatAway"))
+        {
+            Debug.Log(GameObject.FindGameObjectsWithTag("FloatAway"));
+            EndScript.FloatAway(item.transform);
+        }
+        speed = 4.0f;
+        string level = "Level";
+        int score = targetMaster.GetCollectables();
+        MemoryManager.WriteScore2Memory(score);
+        yield return new WaitForSeconds(3.0f);        
+        UI.ShowEndScreen(level, score);
+    }
+
+    private void CheckNextState ()
 	{
 		// Check select to decide what next state is
 		switch (UI.select) {
@@ -431,6 +240,7 @@ public class GameManager : MonoBehaviour
 
 
     float deltaTime = 0.0f;
+    private float speed = 2.0f;
 
     void Update()
     {
@@ -458,6 +268,4 @@ public class GameManager : MonoBehaviour
     {
         Camera.main.GetComponent<MKGlowSystem.MKGlow>().enabled = false;
     }
-
 }
->>>>>>> Dev
