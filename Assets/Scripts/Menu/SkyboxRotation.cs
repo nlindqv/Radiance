16e66e7e02a98eb2f7115a7bcf2c39ba65968301
Victor Bodell <vbodell@kth.se>
2017-05-17 10:31:36 +0200

    Commented away unneccassary printouts

--- Assets/Scripts/Menu/SkyboxRotation.cs
+++ Assets/Scripts/Menu/SkyboxRotation.cs
@@ -8,7 +8,7 @@ public class SkyboxRotation : MonoBehaviour {
 	void Update () {
 		RenderSettings.skybox.SetFloat("_Rotation", 2 * Time.deltaTime + RenderSettings.skybox.GetFloat("_Rotation"));
 		RenderSettings.skybox.SetFloat("_Exposure", Mathf.Sin(Time.deltaTime / 4.0f + RenderSettings.skybox.GetFloat("_Rotation"))/8.0f + 0.7f);
-		Debug.Log(RenderSettings.skybox.GetFloat("_Exposure"));
+//		Debug.Log(RenderSettings.skybox.GetFloat("_Exposure"));
 		//RenderSettings.skybox = skybox;
 
 	}
